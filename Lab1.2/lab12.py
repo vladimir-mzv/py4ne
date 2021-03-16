ba3fc2daee1d1086036669945d6407fdc1a0b34f
vladimir-mzv <vladimir.mzv@gmail.com>
2021-03-16 18:03:27 +0300

    lab 1.2

---
+++ Lab1.2/lab12.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+wb = load_workbook('data_analysis_lab.xlsx')
+def getvalue(x): return x.value
+sheet = wb['Data']
+years = list(map(getvalue, sheet['A'][1:]))
+temp = list(map(getvalue, sheet['C'][1:]))
+act = list(map(getvalue, sheet['D'][1:]))
+pyplot.plot(years, temp)
+pyplot.plot(years, act)
+# pyplot.plot(act, temp)
+pyplot.show()
