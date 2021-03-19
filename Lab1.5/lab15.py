eedac816c6159b7094dd28879c6dbe68ee669f6b
vladimir-mzv <vladimir.mzv@gmail.com>
2021-03-19 16:17:19 +0300

    my lab

---
+++ Lab1.5/lab15.py
@@ -0,0 +1,17 @@
+import os
+from os import walk
+import re
+os.chdir("/home/mzv/config/config_files")
+_, _, filenames = next(walk("."))
+
+#print(filenames)
+
+for i in filenames:
+    #print(i)
+    f = open(i)
+    for j in f:
+      #  print(j)
+        if "ip address " in j:
+         #   print(j)
+            mylist = re.sub('[^0-9,.\ ]','', j)
+            print(mylist)
